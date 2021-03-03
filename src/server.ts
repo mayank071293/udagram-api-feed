a45f2ba5f61e1eed11a783bff31c18c8551d1187
Mayank Bhardwaj <mayank.bhardwaj@mechademy.com>
2021-03-03 12:47:36 +0530

    ADD: dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import {sequelize} from './sequelize';
-
+require('dotenv').config();
 import {IndexRouter} from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
@@ -10,6 +10,7 @@ import {V0_FEED_MODELS} from './controllers/v0/model.index';
 
 
 (async () => {
+  console.log(config);
   await sequelize.addModels(V0_FEED_MODELS);
   //await sequelize.addModels(V0_USER_MODELS);
   await sequelize.sync();
